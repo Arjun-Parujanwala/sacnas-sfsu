baa6e360ab8a4e036fce91a476678364481386ee
Arjun Parujanwala <aparujanwala@gmail.com>
2024-11-14 23:45:28 -0800

    Imported Logo and positioned it

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
 'use client'
-
+import Image from 'next/image'
+import logo from '/public/assets/SACNAS-logo.jpg'
 export default function NavBar() {
 
     const navItems = [
@@ -29,9 +30,10 @@ export default function NavBar() {
         },
     ]
     return (
-        <div className="flex mt-10">
-            <div>
-                <img src="https://sacnas.sfsu.edu/sites/default/files/SACNAS%20Logo%20-%20Full%20Color.png" alt="SACNAS Logo" />
+        <div className="flex mt-10 border-2">
+            <div className='flex px-10 space-x-2'>
+                <Image src={logo.src} alt="SACNAS Logo" width={40} height={40} />
+                <p className='font-helvetica mt-1'>SACNAS AT SFSU</p>
             </div>
             <div className="flex absolute justify-center border-2 space-x-4 px-20 left-1/2 transform -translate-x-1/2 rounded-3xl">
                 {
