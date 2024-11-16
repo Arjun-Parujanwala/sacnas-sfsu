4320c682ef8f68ec73af0a1770a6b52d1be79085
ARJUN PARUJANWALA <aparujanwala@gmail.com>
2024-11-15 21:46:21 -0800

    Changing layout of the NavBar and Title components

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -30,12 +30,12 @@ export default function NavBar() {
         },
     ]
     return (
-        <div className="flex mt-10 border-2">
+        <div className="flex mt-5 border-0">
             <div className='flex px-10 space-x-2'>
                 <Image src={logo.src} alt="SACNAS Logo" width={40} height={40} />
-                <p className='font-helvetica mt-1'>SACNAS AT SFSU</p>
+                <p className='font-helvetica'>SACNAS AT SFSU</p>
             </div>
-            <div className="flex absolute justify-center border-2 space-x-4 px-20 left-1/2 transform -translate-x-1/2 rounded-3xl">
+            <div className="flex absolute justify-content right-5 border-0 space-x-4 px-20 rounded-3xl">
                 {
                     navItems.map((item, index) => {
                         return (
--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,8 +1,14 @@
 export default function Title() {
     return (
-        <div className="flex flex-col justify-center items-center text-center h-screen w-1/2 border-2">
-            <p className="text-9xl">SACNAS</p>
-            <p className="text-9xl">AT SFSU</p>
+        <div className="flex flex-row">
+            <div className="flex flex-col justify-center w-2/3 border-2 font-helvetica mt-10">
+                <p className="flex justify-content text-left float-left text-6xl border-0 pl-20 py-5">SACNAS AT</p>
+                <p className="flex justify-content text-left float-left text-6xl border-0 pl-20 py-5">SAN FRANCISCO STATE</p>
+            </div>
+            <div className="flex flex-col static right-0 border-2 mt-10 py-5">
+                <p className="font-bold">About</p>
+                <p>whatever whatever whatever blah blah blah whatever whatever whatever blah blah blah whatever whatever whatever blah blah blah whatever whatever whatever blah blah blah</p>
+            </div>
         </div>
     )
 }
