88bcfa07166c9aa4c543135db8c806fc884ec333
Arjun Parujanwala <aparujanwala@gmail.com>
2024-11-16 19:31:49 -0800

    Figured out the problem: I was using size rather than font in NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
         <div className="flex flex-wrap mt-5 border-2">
             <div className='flex px-5 space-x-2 border-2 w-full'>
                 <Image src={logo.src} alt="SACNAS Logo" width={40} height={40} className=""/>
-                <p className='font-helvetica size-10'>SACNAS AT SFSU</p>
+                <p className='font-helvetica text-2xl w-full h-full border-2 border-red-600'>SACNAS AT SFSU</p>
             </div>
             <div className="flex flex-wrap justify-content items-center border-2 space-x-3 w-full px-5 font-helvetica">
                 <p>About</p>
