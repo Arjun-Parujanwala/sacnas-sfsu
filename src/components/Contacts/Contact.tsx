dd987d71cc813a947be1fb0fa28a75639c7d1edb
Arjun Parujanwala <aparujanwala@gmail.com>
2024-11-17 19:36:40 -0800

    Made a Contacts section and added placeholders

--- src/components/Contacts/Contact.tsx
+++ src/components/Contacts/Contact.tsx
@@ -1,7 +1,10 @@
 export default function Contacts() {
     return (
-        <div className="border-2 bg">
-            <p>Contacts</p>
+        <div className="flex justify-between bg-purple-800 mx-5 rounded-xl text-white">
+            <p>Facebook</p>
+            <p>Instagram</p>
+            <p>Twitter</p>
+            <p>Discord</p>
         </div>
     )
 }
