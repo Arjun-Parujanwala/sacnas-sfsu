f31cfc821199a165b1260ad487bb7e6b1c93de6d
ARJUN PARUJANWALA <aparujanwala@gmail.com>
2024-11-19 13:22:10 -0800

    Making member cards in members

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -16,7 +16,7 @@ export default function page() {
     {
       id: 2,
       name: 'Arjun Parujanwala',
-      role: 'OutReach/Tech',
+      role: 'Outreach/Tech',
       bio: 'I am the outreach and tech person',
       image: '/public/gray.png',
       grade: 'Sophomore',
@@ -65,18 +65,22 @@ export default function page() {
   ]
   return (
     <main>
-      <div>
-        <h1>Members</h1>
-        <div className='flex '>
-          {members.map((member) => (
-            <div key={member.id} className='border-2'>
-              <Image src={member.image} alt={member.name} width={150} height={150} className='bg-gray-400'/>
-              <h2>{member.name}</h2>
-              <p>{member.role}</p>
-              <p>{member.bio}</p>
-              <p>{member.grade}</p>
-            </div>
-          ))}
+      <div className="flex justify-center">
+        <div className="w-full max-w-6xl p-4">
+          <h1 className="text-center text-4xl font-bold mb-8">Members</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            {members.map((member) => (
+              <div key={member.id} className="border-2 w-72 h-96 mt-10 mx-auto rounded-3xl flex flex-col items-center">
+                <div className="flex justify-center items-center w-full h-48">
+                  <Image src={member.image} alt={member.name} width={150} height={150} className="bg-gray-400 rounded-full"/>
+                </div>
+                <h2 className="text-center text-xl font-semibold mt-4">{member.name}</h2>
+                <p className="text-center">{member.role}</p>
+                <p className="text-center">{member.bio}</p>
+                <p className="text-center">{member.grade}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </main>
