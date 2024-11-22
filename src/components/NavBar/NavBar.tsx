7fb2a1fa236d3975261c15d61ef7ae6923f81331
ARJUN PARUJANWALA <aparujanwala@gmail.com>
2024-11-21 16:38:47 -0800

    Centered the text in menu and created an animation to slide right

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,19 +3,17 @@ import React, { useState } from 'react'
 import styles from './NavBar.module.css'
 import Link from 'next/link'
 
-//Ideally I want to style with TailwindCSS (and A can), but unfortunately doing it for the NavBar too hard.
 export default function NavBar() {
-
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const toggleMenu = (): void => {
         setIsMenuOpen(!isMenuOpen);
-      };
+    };
 
     return (
         <div className="flex justify-end">
             <div className={styles.navbar}>
-                <div className={`${styles.menu} ${isMenuOpen ? styles.active : ''}`} id="menu">
+                <div className={`${styles.menu} ${isMenuOpen ? styles.active : styles.inactive}`} id="menu">
                     <Link href="/">Home</Link>
                     <Link href="/members">Members</Link>
                     <Link href="/events">Events</Link>
@@ -27,6 +25,5 @@ export default function NavBar() {
                 </button>
             </div>
         </div>
-
     )
 }
