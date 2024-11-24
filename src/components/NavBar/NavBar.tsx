0d37cafa8647721509b9c446e629378c66f34145
Arjun Parujanwala <aparujanwala@gmail.com>
2024-11-24 11:19:52 -0800

    NavBar and Load NavBar will only disappear when page is loaded NavBar styled a bit

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,23 +1,39 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './NavBar.module.css'
 import Link from 'next/link'
 
 export default function NavBar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isPageLoaded, setIsPageLoaded] = useState(false);
 
     const toggleMenu = (): void => {
-        setIsMenuOpen(!isMenuOpen);
+        if (isPageLoaded) {
+            setIsMenuOpen(!isMenuOpen);
+        }
     };
+
+    useEffect(() => {
+        const handlePageLoad = () => {
+            setIsPageLoaded(true);
+        };
+
+        if (document.readyState === 'complete') {
+            handlePageLoad();
+        } else {
+            window.addEventListener('load', handlePageLoad);
+            return () => window.removeEventListener('load', handlePageLoad);
+        }
+    }, []);
 
     return (
         <div className="flex justify-end">
             <div className={styles.navbar}>
                 <div className={`${styles.menu} ${isMenuOpen ? styles.active : styles.inactive}`} id="menu">
-                    <Link href="/">Home</Link>
-                    <Link href="/members">Members</Link>
-                    <Link href="/events">Events</Link>
-                    <Link href="/photos">Photos</Link>
+                    <Link href="/" onClick={toggleMenu}>Home</Link>
+                    <Link href="/members" onClick={toggleMenu}>Members</Link>
+                    <Link href="/events" onClick={toggleMenu}>Events</Link>
+                    <Link href="/photos" onClick={toggleMenu}>Photos</Link>
                     <p>Contact</p>
                 </div>
                 <button className={styles.hamburger} onClick={toggleMenu}>
